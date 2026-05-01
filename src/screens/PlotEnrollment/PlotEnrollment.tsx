61cec34857629646a518443c5a77739ad78198ee
Abhishek Pawar <apawar1@intraedge.com>
2026-05-01 18:17:25 +0530

    implmentation of plot mapping with draw border

--- src/screens/PlotEnrollment/PlotEnrollment.tsx
+++ src/screens/PlotEnrollment/PlotEnrollment.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Box, Button, Chip, IconButton, MenuItem, Select, Step, StepLabel, Stepper, TextField } from '@mui/material';
 import styles from './PlotEnrollment.module.scss';
 import { ArrowBack } from '@mui/icons-material'
@@ -12,13 +12,15 @@ import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useNavigate } from 'react-router-dom';
 import { getStepLabels, steps, } from './utils';
+import {MapContainer,Marker,Polygon,Polyline,TileLayer,useMapEvents} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 type Coordinates = { longitude: number, latitude: number }
 
 const PlotEnrollment = () => {
     const navigate = useNavigate();
     const [currentStep, setCurrentStep] = useState(0);
-    const [coordinates, setCoordinates] = useState<Coordinates[]>([]);
+    const [coordinates,] = useState<Coordinates[]>([]);
     const [isEnrollmentSucceded, setIsEnrollmentSucceded] = useState(false);
 
     const stepLabels = useMemo(() => getStepLabels(currentStep), [currentStep])
@@ -28,30 +30,7 @@ const PlotEnrollment = () => {
         if (isLastStep) setIsEnrollmentSucceded(true)
         else setCurrentStep((prev) => prev + 1);
     }
-    const getLocation = () => {
-        if (!navigator.geolocation) {
-            //   setError("Geolocation not supported");
-            console.log('error in location')
-            return;
-        }
-
-        navigator.geolocation.getCurrentPosition(
-            (pos) => {
-                console.log('location', pos)
-                setCoordinates([...coordinates, { longitude: pos.coords.longitude, latitude: pos.coords.latitude }])
-            },
-            (err) => {
-                console.log('error in location', err)
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 10000
-            }
-        );
-    };
-
-
-
+    
 
     return <>
         <Box className={styles.header}>
@@ -113,27 +92,9 @@ const PlotEnrollment = () => {
                                     <CustomDatePicker />
                                 </Box>
                             </>}
-                            {currentStep === 2 && <Box>
-                                <Button fullWidth className={styles.next} onClick={getLocation}>Get Location</Button>
-                                {coordinates.length > 0 && <Box className={styles.coordinates}>
-                                    <Box className={styles.selected}>Selected Coordinates</Box>
-                                    {coordinates.map((coordinate) => <Box className={styles.points}> <Box className={styles.area}>
-                                        <Box className={styles.label}>Latitude</Box>
-                                        <Box className={styles.value}>{coordinate.latitude}</Box>
-                                    </Box>
-                                        <Box className={styles.area}>
-                                            <Box className={styles.label}>Longitude</Box>
-                                            <Box className={styles.value}>{coordinate.longitude}</Box>
-                                        </Box>
-                                    </Box>
-                                    )}
-
-                                    <Box >
-
-
-                                    </Box>
-                                </Box>}
-                            </Box>}
+                            {currentStep === 2 && <>
+                                <MapPlotCoordinates></MapPlotCoordinates>
+                            </>}
 
 
                         </Box>
@@ -181,10 +142,83 @@ const PlotEnrollment = () => {
 
 };
 
+const MapClickHandler = ({ onAddPoint,isFinished }:any) => {
+  useMapEvents({
+    click(e) {
+     if(isFinished)return;
+      const { lat, lng } = e.latlng;
+      onAddPoint({ lat, lng });
+    },
+  });
+
+  return null; // no UI
+};
+const MapPlotCoordinates = () => {
+
+ const coordinates:[number,number][] = [
+//   [18.6298, 73.7997],
+//   [18.63, 73.80],
+//   [18.628, 73.802],
+];
+const [points, setPoints] = useState(coordinates);
+const [center,setCenter] = useState<[number, number]|undefined>(undefined)
+const [isFinished,setIsFinished] = useState(false);
+const handleAddPoint = (point:[number,number])=>{
+    setPoints(prev=>[...prev,point])
+}
+
+const handleSaveUndo = ()=>{
+    if(isFinished){
+        setPoints([]);
+    }
+    setIsFinished(!isFinished);
+}
+
+ useEffect(() => {
+    if (!navigator.geolocation) {
+      console.log("Geolocation not supported");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCenter([latitude, longitude]);
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+        // fallback location
+        setCenter([18.6298, 73.7997]);
+      }
+    );
+  }, []);
+
+  return (<>
+    <MapContainer
+      center={center}
+      zoom={13}
+      maxZoom={18}
+      style={{ height: "500px", width: "100%",borderRadius:'0.5rem' }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MapClickHandler onAddPoint={handleAddPoint} isFinished={isFinished} />
+      {points.map((p, i) => (
+        <Marker key={i} position={p} /> 
+      ))}
+      {(points.length >1 && !isFinished)&& <Polyline positions={points} />}
+      {isFinished && <Polygon positions={points} />}
+    </MapContainer>
+    <Button fullWidth className={styles.next} onClick={handleSaveUndo} disabled={(points.length<3)}>{(!isFinished) ?'Save Coordinates':'Undo'}</Button>
+    </>
+  );
+};
+
 
 
 const CustomTextfield = () => {
-
     return <TextField
         fullWidth
         placeholder='Enter Here'
@@ -195,7 +229,7 @@ const CustomTextfield = () => {
                 borderRadius: '0.75rem',
                 '& fieldset': { border: 'none' },
                 '&:hover fieldset': { border: '1px solid #4CAF50' },
-                '&.Mui-focused fieldset': { border: '1px solid #0B6B2E' },
+                '&.Mui-focused fieldset': { border: '2px solid #0B6B2E' },
             },
         }}
     />
@@ -211,7 +245,7 @@ const CustomSelect = () => {
             borderRadius: '0.75rem',
             '& .MuiOutlinedInput-notchedOutline': { border: 'none' },
             '&:hover .MuiOutlinedInput-notchedOutline': { border: '1px solid #4CAF50' },
-            '&.Mui-focused .MuiOutlinedInput-notchedOutline': { border: '1px solid #0B6B2E' },
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': { border: '2px solid #0B6B2E' },
         }}
         onChange={(event) => setValue(event.target.value)}
         renderValue={(selected) => {
@@ -232,24 +266,26 @@ const CustomDatePicker = () => {
                 value={value}
                 onChange={(newValue) => setValue(newValue)}
                 sx={{
-                    '& .MuiPickersOutlinedInput-root': {
-                        background: '#efefeb',
-                        borderRadius: '0.75rem',
-                    },
-                    '& .MuiPickersOutlinedInput-root .MuiPickersOutlinedInput-notchedOutline': {
-                        border: 'none',
-                    },
-                    '& .MuiPickersOutlinedInput-root:hover .MuiPickersOutlinedInput-notchedOutline': {
-                        border: '1px solid #4CAF50',
-                    },
-                    // Add the parent class to increase specificity
-                    '& .MuiPickersTextField-root .MuiPickersOutlinedInput-root.Mui-focused .MuiPickersOutlinedInput-notchedOutline': {
-                        border: '1px solid #0B6B2E',
-                    },
-                    // Also override the direct MUI variable that drives the blue color
-                    '& .MuiPickersOutlinedInput-root.Mui-focused': {
-                        '--mui-palette-primary-main': '#0B6B2E',
-                    },
+                    '&.MuiFormControl-root.MuiPickersTextField-root': {
+                        '& .MuiPickersInputBase-root.MuiPickersOutlinedInput-root': {
+                            backgroundColor: '#efefeb',
+                            borderRadius: '0.75rem',
+                            '& .MuiPickersOutlinedInput-notchedOutline': {
+                                border: 'none',
+                                outline: 'none',
+                                // backgroundColor:'#efefeb',
+
+                            },
+                            '&:hover .MuiPickersOutlinedInput-notchedOutline': {
+
+                                border: '1px solid #4CAF50'
+                            },
+                            '&.Mui-focused .MuiPickersOutlinedInput-notchedOutline': {
+                                border: '2px solid #0B6B2E'
+                            },
+                        }
+                    }
+
                 }}
                 slotProps={{
                     popper: {
